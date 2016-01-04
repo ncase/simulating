b16ae7c749933ede67874c7e8289f520dd170b57
Nicky Case <n@ncase.me>
2016-01-04 11:18:22 -0800

    fallback for mac os 10.6 and lower

--- scripts/emojiTest.js
+++ scripts/emojiTest.js
@@ -2,15 +2,22 @@
 
 // SCREW IT.
 // According to Can I Emoji: http://caniemoji.com/
-// Mac: EVERYTHING WORKS
+// Mac: EVERYTHING WORKS for 10.7 and higher
 // Windows: FAIL ON VISTA/XP, otherwise EVERYTHING EXCEPT CHROME
 // iOS & Andoid & Windows Phone: YES BUT THIS ISN'T MOBILE-FRIENDLY
 // Linux/Everything Else: SORRY
 
 exports.IfDoesNotSupportEmoji = function(fail){
 
-	// Mac: EVERYTHING WORKS
-	if(platform.os.family=="OS X") return;
+	// Mac: EVERYTHING WORKS for 10.7 and higher
+	if(platform.os.family=="OS X"){
+		var version = parseInt(platform.os.version.split(".")[1]);
+		if(version>=7){
+			return;
+		}else{
+			fail(); return;	
+		}
+	}
 
 	// Windows: FAIL ON VISTA/XP, otherwise EVERYTHING EXCEPT CHROME
 	var isWindows = (platform.os.family.search("Windows")>=0); // it contains "Windows" somewhere
